b9e28805ab0b969d5dccdfb649ea7c71d8d74ac8
kimpie <kapienta@gmail.com>
2013-11-26 15:16:55 +1100

    testing auth

--- public/js/views/facebook.js
+++ public/js/views/facebook.js
@@ -9,7 +9,7 @@ window.fbAsyncInit = function() {
 
     // Additional initialization code such as adding Event Listeners goes here
 
-    FB.getLoginStatus(function(response) {
+  /*  FB.getLoginStatus(function(response) {
   if (response.status === 'connected') {
     // the user is logged in and has authenticated your
     // app, and response.authResponse supplies
@@ -18,6 +18,8 @@ window.fbAsyncInit = function() {
     // and signed request each expire
     var uid = response.authResponse.userID;
     var accessToken = response.authResponse.accessToken;
+    $(".loggedin").css('display', 'block');
+    $(".loggedoff").css('display', 'none');
   } else if (response.status === 'not_authorized') {
     // the user is logged in to Facebook, 
     // but has not authenticated your app
@@ -25,7 +27,37 @@ window.fbAsyncInit = function() {
     // the user isn't logged in to Facebook.
     window.top.location = 'https://www.facebook.com/index.php';
   }
- }); //FB.getLoginStatus
+ }); //FB.getLoginStatus*/
+
+// Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+  // for any authentication related change, such as login, logout or session refresh. This means that
+  // whenever someone who was previously logged out tries to log in again, the correct case below 
+  // will be handled. 
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    // Here we specify what we do with the response anytime this event occurs. 
+    if (response.status === 'connected') {
+      // The response object is returned with a status field that lets the app know the current
+      // login status of the person. In this case, we're handling the situation where they 
+      // have logged in to the app.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // In this case, the person is logged into Facebook, but not into the app, so we call
+      // FB.login() to prompt them to do so. 
+      // In real-life usage, you wouldn't want to immediately prompt someone to login 
+      // like this, for two reasons:
+      // (1) JavaScript created popup windows are blocked by most browsers unless they 
+      // result from direct interaction from people using the app (such as a mouse click)
+      // (2) it is a bad experience to be continually prompted to login upon page load.
+    } else {
+      // In this case, the person is not logged into Facebook, so we call the login() 
+      // function to prompt them to do so. Note that at this stage there is no indication
+      // of whether they are logged into the app. If they aren't then they'll see the Login
+      // dialog right after they log in to Facebook. 
+      // The same caveats as above apply to the FB.login() call here.
+      FB.login();
+    }
+  });
+
 };  //fbAsyngInit
 
 function goLogIn() {
@@ -56,6 +88,15 @@ function goLogIn() {
  // Insert the Facebook JS SDK into the DOM
  firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
 }());
+
+// Here we run a very simple test of the Graph API after login is successful. 
+  // This testAPI() function is only called in those cases. 
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  };
 
 //Start Game code
 jQuery(document).ready(function () {
