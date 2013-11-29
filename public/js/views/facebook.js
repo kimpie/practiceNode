55b906b097550071baafce019a83afde80793aa5
kimpie <kapienta@gmail.com>
2013-11-29 15:52:11 +1100

    added the FB Request Dialog

--- public/js/views/facebook.js
+++ public/js/views/facebook.js
@@ -9,27 +9,7 @@ window.fbAsyncInit = function() {
 
     // Additional initialization code such as adding Event Listeners goes here
 
-  /*  FB.getLoginStatus(function(response) {
-  if (response.status === 'connected') {
-    // the user is logged in and has authenticated your
-    // app, and response.authResponse supplies
-    // the user's ID, a valid access token, a signed
-    // request, and the time the access token 
-    // and signed request each expire
-    var uid = response.authResponse.userID;
-    var accessToken = response.authResponse.accessToken;
-    $(".loggedin").css('display', 'block');
-    $(".loggedoff").css('display', 'none');
-  } else if (response.status === 'not_authorized') {
-    // the user is logged in to Facebook, 
-    // but has not authenticated your app
-  } else {
-    // the user isn't logged in to Facebook.
-    window.top.location = 'https://www.facebook.com/index.php';
-  }
- }); //FB.getLoginStatus*/
-
-// Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+  // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
   // for any authentication related change, such as login, logout or session refresh. This means that
   // whenever someone who was previously logged out tries to log in again, the correct case below 
   // will be handled. 
@@ -40,6 +20,8 @@ window.fbAsyncInit = function() {
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
       testAPI();
+      $(".loggedin").css('display', 'block');
+      $(".loggedoff").css('display', 'none');
     } else if (response.status === 'not_authorized') {
       // In this case, the person is logged into Facebook, but not into the app, so we call
       // FB.login() to prompt them to do so. 
@@ -48,27 +30,19 @@ window.fbAsyncInit = function() {
       // (1) JavaScript created popup windows are blocked by most browsers unless they 
       // result from direct interaction from people using the app (such as a mouse click)
       // (2) it is a bad experience to be continually prompted to login upon page load.
+      $(".loggedin").css('display', 'none');
+      $(".loggedoff").css('display', 'block');
     } else {
       // In this case, the person is not logged into Facebook, so we call the login() 
       // function to prompt them to do so. Note that at this stage there is no indication
       // of whether they are logged into the app. If they aren't then they'll see the Login
       // dialog right after they log in to Facebook. 
       // The same caveats as above apply to the FB.login() call here.
-      FB.login();
+      $(".loggedin").css('display', 'none');
+      $(".loggedoff").css('display', 'block');
     }
   });
-
 };  //fbAsyngInit
-
-function goLogIn() {
-  FB.login(function(response){
-    //handle the response
-    $(".loggedin").css('display', 'block');
-    $(".loggedoff").css('display', 'none');
-  }, {scope: 'email, user_likes'}); //FB.login
-
-}; //goLogIn
-
 
   // Load the SDK asynchronously
 (function(){
@@ -91,14 +65,25 @@ function goLogIn() {
 
 // Here we run a very simple test of the Graph API after login is successful. 
   // This testAPI() function is only called in those cases. 
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  };
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(info) {
+    console.log(info);
+  });
+};
 
-//Start Game code
+function requestDialog() {
+  FB.ui({method: 'apprequests',
+     message: 'Play Complete the Sentence game with me! It\'s hilaious!' 
+    });
+};
+
+
+//End of Facebook Scripts
+
+
+
+//Begining of game scripts
 jQuery(document).ready(function () {
   var log_chat_message = function (message, type) {
     var li = jQuery('<li />').text(message);
@@ -114,16 +99,22 @@ jQuery(document).ready(function () {
     } 
     jQuery('#chat_log').append(li);
   };
+  //REQUEST DIALOG
+  jQuery('#social').click(requestDialog);
 
-  var display_id = function (name, currentUser) {
-    var name = jQuery('<ul />').text(name + "  ");
-    jQuery('#display_name').append(name);
+  jQuery('#console').click(newGame);
+
+  var display_id = function testAPI() {
+    FB.api('/me', function(info) {
+      jQuery('#display_name').append(info.first_name);
+    });
   };
 
-  jQuery('.loggedoff').click(goLogIn);
+  function newGame () {
+    //enter info here to start rooms/authorization
+  };
 
-  
-  var socket = io.connect('http://completethesentence.com/');
+  var socket = io.connect('https://completethesentence.com/');
   //var socket = io.connect('http://localhost');
 
 
